bcbbf44f2dbf3a82512bdccb0977dc31c82f0a9b
Sergei8720 <162614614+Sergei8720@users.noreply.github.com>
2026-01-18 01:05:20 +0200

    Add files via upload

--- sprint1/problems/final_task/solution/src/json_converter.cpp
+++ sprint1/problems/final_task/solution/src/json_converter.cpp
@@ -36,7 +36,7 @@ Json::Value BuildingToJson(const model::Building& building) {
 Json::Value OfficeToJson(const model::Office& office) {
     Json::Value json_office;
     
-    json_office["id"] = (*office.GetId()).c_str();
+    json_office["id"] = *office.GetId();
     json_office["x"] = office.GetPosition().x;
     json_office["y"] = office.GetPosition().y;
     json_office["offsetX"] = office.GetOffset().dx;
@@ -47,17 +47,12 @@ Json::Value OfficeToJson(const model::Office& office) {
 
 std::string CreateErrorResponse(const std::string& code, const std::string& message) {
     Json::Value root;
-    root["code"] = code.c_str();
-    root["message"] = message.c_str();
+    root["code"] = code;
+    root["message"] = message;
     
     Json::StreamWriterBuilder builder;
     builder["indentation"] = "";
-    std::ostringstream stream;
-    Json::StreamWriter* writer = builder.newStreamWriter();
-    writer->write(root, &stream);
-    delete writer;
-    
-    return stream.str();
+    return Json::writeString(builder, root);
 }
 
 }  // namespace
@@ -67,26 +62,21 @@ std::string ConvertMapListToJson(const model::Game& game) {
     
     for (const auto& map : game.GetMaps()) {
         Json::Value json_map;
-        json_map["id"] = (*map.GetId()).c_str();
-        json_map["name"] = map.GetName().c_str();
+        json_map["id"] = *map.GetId();
+        json_map["name"] = map.GetName();
         root.append(json_map);
     }
     
     Json::StreamWriterBuilder builder;
     builder["indentation"] = "";
-    std::ostringstream stream;
-    Json::StreamWriter* writer = builder.newStreamWriter();
-    writer->write(root, &stream);
-    delete writer;
-    
-    return stream.str();
+    return Json::writeString(builder, root);
 }
 
 std::string ConvertMapToJson(const model::Map& map) {
     Json::Value root;
     
-    root["id"] = (*map.GetId()).c_str();
-    root["name"] = map.GetName().c_str();
+    root["id"] = *map.GetId();
+    root["name"] = map.GetName();
     
     Json::Value roads(Json::arrayValue);
     for (const auto& road : map.GetRoads()) {
@@ -108,12 +98,7 @@ std::string ConvertMapToJson(const model::Map& map) {
     
     Json::StreamWriterBuilder builder;
     builder["indentation"] = "";
-    std::ostringstream stream;
-    Json::StreamWriter* writer = builder.newStreamWriter();
-    writer->write(root, &stream);
-    delete writer;
-    
-    return stream.str();
+    return Json::writeString(builder, root);
 }
 
 std::string CreateMapNotFoundResponse() {
