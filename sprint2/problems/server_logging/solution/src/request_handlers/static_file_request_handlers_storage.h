45cd38893986566f12f91dbf6cd959cf0427aa53
Sergei8720 <162614614+Sergei8720@users.noreply.github.com>
2026-02-12 01:20:50 +0200

    static_file_request_handlers_storage.h

--- sprint2/problems/server_logging/solution/src/request_handlers/static_file_request_handlers_storage.h
+++ sprint2/problems/server_logging/solution/src/request_handlers/static_file_request_handlers_storage.h
@@ -1,165 +1,126 @@
 #pragma once
-#include "filesystem_utils.h"
-#include "logger.h"
-#include <vector>
-#include <boost/beast/http.hpp>
-#include <unordered_map>
-#include <string>
+
+#include "static_file_request_handlers.h"
+#include "static_file_handler_helpers.h"
+
+#include <filesystem>
 #include <fstream>
+#include <string>
 
 namespace rh_storage {
 
 namespace beast = boost::beast;
 namespace http = beast::http;
-namespace sys = boost::system;
-namespace fs = std::filesystem;
-using StringResponse = http::response<http::string_body>;
-using namespace std::literals;
 
-const std::unordered_map<std::string, std::string> ExtensionFileToContentType = {
-    {".htm", "text/html"},
-    {".html", "text/html"},
-    {".css", "text/css"},
-    {".txt", "text/plain"},
-    {".js", "text/javascript"},
-    {".json", "application/json"},
-    {".xml", "application/xml"},
-    {".png", "image/png"},
-    {".jpg", "image/jpeg"},
-    {".jpe", "image/jpeg"},
-    {".jpeg", "image/jpeg"},
-    {".gif", "image/gif"},
-    {".bmp", "image/bmp"},
-    {".ico", "image/vnd.microsoft.icon"},
-    {".tiff", "image/tiff"},
-    {".tif", "image/tiff"},
-    {".svg", "image/svg+xml"},
-    {".svgz", "image/svg+xml"},
-    {".mp3", "audio/mpeg"}
-};
+inline bool StaticContentFileNotFoundActivator(const http::request<http::string_body>& req,
+                                               const std::filesystem::path& static_content_path) {
+    namespace fs = std::filesystem;
 
-const std::string IndexFileName = "index.html";
-
-template <typename Request>
-bool StaticContentFileNotFoundActivator(const Request& req, const fs::path& static_content_root) {
-    fs::path static_content = static_content_root;
-    
-    std::string target(req.target());
-    if (target.empty() || target == "/") {
-        static_content = fs::weakly_canonical(static_content_root / IndexFileName);
-    } else {
-        std::string path_str;
-        if (target[0] == '/') {
-            path_str = target.substr(1);
-        } else {
-            path_str = target;
-        }
-        fs::path rel_path(path_str);
-        static_content = fs::weakly_canonical(static_content_root / rel_path);
+    auto target = req.target();
+    if (target.starts_with("/static/")) {
+        fs::path relative_path = std::string(target.substr(8));
+        fs::path full_path = static_content_path / relative_path;
+        full_path = fs::weakly_canonical(full_path);
         
-        if (fs::is_directory(static_content)) {
-            static_content = fs::weakly_canonical(static_content / IndexFileName);
-        }
+        return !fs::exists(full_path) || !fs::is_regular_file(full_path);
     }
-    
-    return !fs::exists(static_content);
+    return false;
 }
 
-template <typename Request, typename Send>
-void StaticContentFileNotFoundHandler(const Request& req, const fs::path& static_content_root, Send&& send) {
-    StringResponse response(http::status::not_found, req.version());
-    response.set(http::field::content_type, "text/plain");
-    response.body() = "Static content file not found.";
-    response.content_length(response.body().size());
-    response.keep_alive(req.keep_alive());
-    send(response);
+template <typename Send>
+void StaticContentFileNotFoundHandler(const http::request<http::string_body>& req,
+                                      const std::filesystem::path& /*static_content_path*/,
+                                      Send&& send) {
+    std::string body = "File not found";
+    http::response<http::string_body> res{http::status::not_found, req.version()};
+    res.set(http::field::content_type, "text/plain");
+    res.body() = body;
+    res.prepare_payload();
+    send(std::move(res));
 }
 
-template <typename Request>
-bool LeaveStaticContentRootDirActivator(const Request& req, const fs::path& static_content_root) {
-    std::string target(req.target());
-    if (target.empty() || target == "/") {
-        return false;
+inline bool LeaveStaticContentRootDirActivator(const http::request<http::string_body>& req,
+                                               const std::filesystem::path& static_content_path) {
+    namespace fs = std::filesystem;
+
+    auto target = req.target();
+    if (target.starts_with("/static/")) {
+        fs::path relative_path = std::string(target.substr(8));
+        fs::path full_path = static_content_path / relative_path;
+        full_path = fs::weakly_canonical(full_path);
+        
+        return !full_path.string().starts_with(static_content_path.string());
     }
-    
-    std::string path_str;
-    if (target[0] == '/') {
-        path_str = target.substr(1);
-    } else {
-        path_str = target;
-    }
-    
-    fs::path rel_path(path_str);
-    fs::path static_content = fs::weakly_canonical(static_content_root / rel_path);
-    
-    return !fs_utils::IsSubPath(static_content, static_content_root);
+    return false;
 }
 
-template <typename Request, typename Send>
-void LeaveStaticContentRootDirHandler(const Request& req, const fs::path& static_content_root, Send&& send) {
-    StringResponse response(http::status::bad_request, req.version());
-    response.set(http::field::content_type, "text/plain");
-    response.body() = "Try to leave static content root directory.";
-    response.content_length(response.body().size());
-    response.keep_alive(req.keep_alive());
-    send(response);
+template <typename Send>
+void LeaveStaticContentRootDirHandler(const http::request<http::string_body>& req,
+                                      const std::filesystem::path& /*static_content_path*/,
+                                      Send&& send) {
+    std::string body = "Forbidden";
+    http::response<http::string_body> res{http::status::forbidden, req.version()};
+    res.set(http::field::content_type, "text/plain");
+    res.body() = body;
+    res.prepare_payload();
+    send(std::move(res));
 }
 
-template <typename Request>
-bool GetStaticContentFileActivator(const Request& req, const fs::path& static_content_root) {
-    return true;
+inline bool GetStaticContentFileActivator(const http::request<http::string_body>& req,
+                                          const std::filesystem::path& static_content_path) {
+    namespace fs = std::filesystem;
+
+    auto target = req.target();
+    if (target.starts_with("/static/")) {
+        fs::path relative_path = std::string(target.substr(8));
+        fs::path full_path = static_content_path / relative_path;
+        full_path = fs::weakly_canonical(full_path);
+        
+        return fs::exists(full_path) && fs::is_regular_file(full_path) &&
+               full_path.string().starts_with(static_content_path.string());
+    }
+    return false;
 }
 
-template <typename Request, typename Send>
-void GetStaticContentFileHandler(const Request& req, const fs::path& static_content_root, Send&& send) {
-    http::response<http::file_body> response;
-    response.version(11);
-    response.result(http::status::ok);
+template <typename Send>
+void GetStaticContentFileHandler(const http::request<http::string_body>& req,
+                                 const std::filesystem::path& static_content_path,
+                                 Send&& send) {
+    namespace fs = std::filesystem;
+    namespace beast = boost::beast;
+    namespace http = beast::http;
 
-    fs::path static_content = static_content_root;
-    
-    std::string target(req.target());
-    if (target.empty() || target == "/") {
-        static_content = fs::weakly_canonical(static_content_root / IndexFileName);
-    } else {
-        std::string path_str;
-        if (target[0] == '/') {
-            path_str = target.substr(1);
-        } else {
-            path_str = target;
-        }
-        fs::path rel_path(path_str);
-        static_content = fs::weakly_canonical(static_content_root / rel_path);
-    }
-    
-    std::string extension = static_content.extension().string();
-    auto it = ExtensionFileToContentType.find(extension);
-    if (it != ExtensionFileToContentType.end()) {
-        response.insert(http::field::content_type, it->second);
-    } else {
-        response.insert(http::field::content_type, "application/octet-stream");
-    }
+    auto target = req.target();
+    fs::path relative_path = std::string(target.substr(8));
+    fs::path full_path = static_content_path / relative_path;
+    full_path = fs::weakly_canonical(full_path);
+
+    http::response<http::file_body> res;
+    res.version(req.version());
+    res.result(http::status::ok);
     
     http::file_body::value_type file;
-    sys::error_code ec;
+    beast::error_code ec;
     
-    file.open(static_content.string(), beast::file_mode::read, ec);
+    // ИСПРАВЛЕНО: используем c_str() для преобразования string в const char*
+    file.open(full_path.string().c_str(), beast::file_mode::read, ec);
+    
     if (ec) {
-        BOOST_LOG_TRIVIAL(error) << logware::CreateLogMessage("error",
-            logware::ExceptionLogData(ec.value(), "Failed to open static content file", ec.message()));
-        
-        StringResponse error_response(http::status::internal_server_error, req.version());
-        error_response.set(http::field::content_type, "text/plain");
-        error_response.body() = "Failed to open file.";
-        error_response.content_length(error_response.body().size());
-        error_response.keep_alive(req.keep_alive());
-        send(error_response);
+        http::response<http::string_body> error_res{http::status::internal_server_error, req.version()};
+        error_res.set(http::field::content_type, "text/plain");
+        error_res.body() = "Failed to open file";
+        error_res.prepare_payload();
+        send(std::move(error_res));
         return;
     }
     
-    response.body() = std::move(file);
-    response.prepare_payload();
-    send(response);
+    res.body() = std::move(file);
+    res.prepare_payload();
+    
+    auto mime_type = http_handler::GetMimeType(full_path.extension().string());
+    res.set(http::field::content_type, mime_type);
+    
+    send(std::move(res));
 }
 
-}
+} // namespace rh_storage
