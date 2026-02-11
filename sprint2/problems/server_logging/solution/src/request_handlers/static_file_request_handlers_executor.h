8df8da5dcf9228441973ec30ae3dacc1eed15bd4
Sergei8720 <162614614+Sergei8720@users.noreply.github.com>
2026-02-12 01:27:27 +0200

    static_file_request_handlers_executor.h

--- sprint2/problems/server_logging/solution/src/request_handlers/static_file_request_handlers_executor.h
+++ sprint2/problems/server_logging/solution/src/request_handlers/static_file_request_handlers_executor.h
@@ -1,50 +1,60 @@
 #pragma once
-#include "static_file_request_handlers_storage.h"
+
 #include "request_handler_node.h"
-#include <functional>
+#include "static_file_request_handlers_storage.h"
+
 #include <vector>
+#include <unordered_map>
+#include <filesystem>
 
 namespace rh_storage {
 
-template<typename Request, typename Send>
+namespace http = boost::beast::http;
+
+template <typename Request, typename Send>
 class StaticFileRequestHandlerExecutor {
-    using ActivatorType = bool(*)(const Request&, const std::filesystem::path&);
-    using HandlerType = void(*)(const Request&, const std::filesystem::path&, Send&&);
-    
 public:
-    StaticFileRequestHandlerExecutor(const StaticFileRequestHandlerExecutor&) = delete;
-    StaticFileRequestHandlerExecutor& operator=(const StaticFileRequestHandlerExecutor&) = delete;
-    StaticFileRequestHandlerExecutor(StaticFileRequestHandlerExecutor&&) = delete;
-    StaticFileRequestHandlerExecutor& operator=(StaticFileRequestHandlerExecutor&&) = delete;
+    using HandlerType = void(*)(const Request&, const std::filesystem::path&, Send&&);
+    using ActivatorType = bool(*)(const Request&, const std::filesystem::path&);
 
     static StaticFileRequestHandlerExecutor& GetInstance() {
-        static StaticFileRequestHandlerExecutor obj;
-        return obj;
+        static StaticFileRequestHandlerExecutor instance;
+        return instance;
     }
 
-    bool Execute(const Request& req, const std::filesystem::path& static_content_root, Send&& send) {
-        for (auto& item : rh_storage_) {
-            if (item.GetActivator()(req, static_content_root)) {
-                item.GetHandler(req, fault_handler_)(req, static_content_root, std::move(send));
-                return true;
+    bool Execute(const Request& req, const std::filesystem::path& static_content_path, Send&& send) const {
+        for (const auto& node : rh_storage_) {
+            if (node.activator(req, static_content_path)) {
+                auto it = node.handlers.find(req.method());
+                if (it != node.handlers.end()) {
+                    it->second(req, static_content_path, std::forward<Send>(send));
+                    return true;
+                }
             }
         }
         return false;
     }
 
 private:
-    std::vector<RequestHandlerNode<ActivatorType, HandlerType>> rh_storage_;
-    HandlerType fault_handler_;
+    StaticFileRequestHandlerExecutor() {
+        // ИСПРАВЛЕНО: явно создаем объект RequestHandlerNode
+        rh_storage_.emplace_back(RequestHandlerNode<ActivatorType, HandlerType>{
+            StaticContentFileNotFoundActivator,
+            std::unordered_map<http::verb, HandlerType>{{http::verb::get, StaticContentFileNotFoundHandler<Send>}}
+        });
 
-    StaticFileRequestHandlerExecutor()
-        : fault_handler_(StaticContentFileNotFoundHandler) {
-        rh_storage_.emplace_back(StaticContentFileNotFoundActivator,
-            std::unordered_map<http::verb, HandlerType>{{http::verb::get, StaticContentFileNotFoundHandler}});
-        rh_storage_.emplace_back(LeaveStaticContentRootDirActivator,
-            std::unordered_map<http::verb, HandlerType>{{http::verb::get, LeaveStaticContentRootDirHandler}});
-        rh_storage_.emplace_back(GetStaticContentFileActivator,
-            std::unordered_map<http::verb, HandlerType>{{http::verb::get, GetStaticContentFileHandler}});
+        rh_storage_.emplace_back(RequestHandlerNode<ActivatorType, HandlerType>{
+            LeaveStaticContentRootDirActivator,
+            std::unordered_map<http::verb, HandlerType>{{http::verb::get, LeaveStaticContentRootDirHandler<Send>}}
+        });
+
+        rh_storage_.emplace_back(RequestHandlerNode<ActivatorType, HandlerType>{
+            GetStaticContentFileActivator,
+            std::unordered_map<http::verb, HandlerType>{{http::verb::get, GetStaticContentFileHandler<Send>}}
+        });
     }
+
+    std::vector<RequestHandlerNode<ActivatorType, HandlerType>> rh_storage_;
 };
 
-}
+} // namespace rh_storage
