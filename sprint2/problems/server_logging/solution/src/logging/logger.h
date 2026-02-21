dd5818f061906c9028ec61b4bd2ff01d341d8ec7
Sergei8720 <more-02.prosin@yandex.ru>
2026-02-21 22:33:57 +0200

    21
    21

--- sprint2/problems/server_logging/solution/src/logging/logger.h
+++ sprint2/problems/server_logging/solution/src/logging/logger.h
@@ -4,21 +4,30 @@
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/json.hpp>
 #include <string>
 
+#include "logging_data_storage.h"
+
 namespace logware {
+
+namespace logging = boost::log;
+namespace keywords = boost::log::keywords;
+namespace json = boost::json;
+
+using namespace std::literals;
 
 void InitLogger();
 
 template <typename T>
 std::string CreateLogMessage(const std::string& message, const T& data) {
-  boost::json::object log_entry;
+  json::object log_entry;
   log_entry["timestamp"] = boost::posix_time::to_iso_extended_string(
       boost::posix_time::microsec_clock::local_time());
   log_entry["message"] = message;
-  log_entry["data"] = boost::json::value_from(data);
-  return boost::json::serialize(log_entry);
+  log_entry["data"] = json::value_from(data);
+  return json::serialize(log_entry);
 }
 
 }  // namespace logware
