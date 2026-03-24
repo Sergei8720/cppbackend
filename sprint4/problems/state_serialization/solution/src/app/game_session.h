387cdc87bac557753fea0c068189c9b41464a8a8
Sergei8720 <more-02.prosin@yandex.ru>
2026-03-25 00:20:17 +0200

    510
    510

--- sprint4/problems/state_serialization/solution/src/app/game_session.h
+++ sprint4/problems/state_serialization/solution/src/app/game_session.h
@@ -12,7 +12,6 @@
 #include <chrono>
 #include <vector>
 #include <memory>
-#include <chrono>
 #include <unordered_map>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/strand.hpp>
@@ -21,7 +20,7 @@ namespace app {
 
 namespace net = boost::asio;
 
-class Player;  // forward declaration
+class Player;
 
 class GameSession : public std::enable_shared_from_this<GameSession>  {
 public:
@@ -51,7 +50,7 @@ public:
                 ),
                 loot_gen_cfg.probability),
             period_of_update_game_state_(period_of_update_game_state) {
-    };
+    }
     
     void Run();
     
--- sprint4/problems/state_serialization/solution/src/middleware/time_management/ticker.h
+++ sprint4/problems/state_serialization/solution/src/middleware/time_management/ticker.h
@@ -2,6 +2,7 @@
 
 #include "sdk.h"
 #include <chrono>
+#include <functional>
 #include <boost/asio/strand.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/steady_timer.hpp>
@@ -13,27 +14,32 @@ using Strand = boost::asio::strand<boost::asio::io_context::executor_type>;
 template<typename Handler>
 class Ticker : public std::enable_shared_from_this<Ticker<Handler>> {
 public:
+    // Конструктор для использования со strand
     Ticker(Strand strand, std::chrono::milliseconds period, Handler handler) 
-        : period_(period), handler_(handler), strand_(strand) {
+        : period_(period), handler_(handler), strand_(std::make_shared<Strand>(strand)), timer_(*this->strand_) {
     }
     
+    // Конструктор без strand
     Ticker(boost::asio::io_context& ioc, std::chrono::milliseconds period, Handler handler)
-        : period_(period), handler_(handler), timer_(ioc) {
+        : period_(period), handler_(handler), strand_(nullptr), timer_(ioc) {
     }
     
     void Start() {
         if (strand_) {
-            strand_->execute([this] { ScheduleTick(); });
+            boost::asio::dispatch(*strand_, [self = this->shared_from_this()] {
+                self->ScheduleTick();
+            });
         } else {
             ScheduleTick();
         }
     }
     
     void SingleShot() {
+        auto self = this->shared_from_this();
         timer_.expires_after(period_);
-        timer_.async_wait([this](boost::system::error_code ec) {
+        timer_.async_wait([self](boost::system::error_code ec) {
             if (!ec) {
-                handler_();
+                self->handler_();
             }
         });
     }
@@ -41,8 +47,11 @@ public:
 private:
     void ScheduleTick() {
         last_tick_ = std::chrono::steady_clock::now();
+        auto self = this->shared_from_this();
         timer_.expires_after(period_);
-        timer_.async_wait([this](boost::system::error_code ec) { OnTick(ec); });
+        timer_.async_wait([self](boost::system::error_code ec) {
+            self->OnTick(ec);
+        });
     }
     
     void OnTick(boost::system::error_code ec) {
@@ -54,7 +63,7 @@ private:
     }
 
     std::shared_ptr<Strand> strand_;
-    boost::asio::steady_timer timer_{strand_ ? *strand_ : boost::asio::make_strand(timer_.get_executor())};
+    boost::asio::steady_timer timer_;
     std::chrono::milliseconds period_;
     Handler handler_;
     std::chrono::time_point<std::chrono::steady_clock> last_tick_;
